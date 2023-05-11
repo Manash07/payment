c213c6251ab73bba06fee3072775f568142ffcd0
Manash07 <manashghimire2072@gmail.com>
2023-05-11 14:47:20 +0545

    Header and Footer updated and new page started

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,14 +1,10 @@
 const Footer = () => {
   return (
     <>
-      <footer>
-        <div class="card-footer text-body-secondary page-footer">
-          <p className="mx-5 py-2">
-            {" "}
-            Copyright © ChittoPay. All rights reserved
-          </p>
-        </div>
+      <footer className="px-3 py-3">
+        Copyright © ChittoPay. All rights reserved
       </footer>
+      {/** Footer bottom problem was solved by keeping the position fixed in css. */}
     </>
   );
 };
---
+++ client/src/pages/mockkyc/index.js
@@ -0,0 +1,121 @@
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
+const Kycform = () => {
+  return (
+    <>
+      <Header />
+      <section className="kycform">
+        <div className="container">
+          <h5> Please fill your KYC Form </h5>
+          <div className="my-4 h-80 d-flex justify-content-start">
+            <div className="card" style={{ width: "45rem" }}>
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-md-6 col-lg-6 col-sm-12">
+                    <img
+                      src="/Icon.png"
+                      className="img-thumbnail my-3"
+                      alt="..."
+                      style={{ width: "10rem", height: "10rem" }}
+                    />
+                    <div className="input-group flex-nowrap">
+                      <input
+                        type="text"
+                        className="form-control my-3"
+                        placeholder="Enter your name written in citizenship"
+                        aria-label="Username"
+                        aria-describedby="addon-wrapping"
+                      />
+                    </div>
+                    <div className="input-group flex-nowrap">
+                      <input
+                        type="number"
+                        className="form-control my-3"
+                        placeholder="Enter your personal mobile number"
+                        aria-label="Username"
+                        aria-describedby="addon-wrapping"
+                      />
+                    </div>
+                    <div className="input-group flex-nowrap">
+                      <select
+                        className="form-select my-3"
+                        aria-label="Default select example"
+                      >
+                        <option selected>Choose your bank</option>
+                        <option>Nepal Investment Mega Bank</option>
+                        <option>Sunrise Bank</option>
+                        <option>Nepal Bank Limited</option>
+                      </select>
+                    </div>
+                    <div className="input-group flex-nowrap">
+                      <input
+                        type="number"
+                        className="form-control my-3"
+                        placeholder="Enter your account number"
+                        aria-label="Username"
+                        aria-describedby="addon-wrapping"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-md-6 col-lg-6 col-sm-12">
+                    <div className="input-group flex-nowrap">
+                      <select
+                        className="form-select my-3"
+                        aria-label="Default select example"
+                      >
+                        <option selected>Choose your branch</option>
+                        <option>Kumaripati, Lalitpur</option>
+                        <option>Nakkhu, Lalitpur</option>
+                        <option>Newroad, Kathmandu</option>
+                      </select>
+                    </div>
+
+                    <div className="input-group flex-nowrap">
+                      <select
+                        className="form-select my-3"
+                        aria-label="Default select example"
+                      >
+                        <option selected>Choose your document type</option>
+                        <option>License</option>
+                        <option>Citizenship</option>
+                        <option>
+                          National Identity Card
+                        </option>
+                      </select>
+                    </div>
+
+                    <div className="input-group flex-nowrap">
+                      <select
+                        className="form-select my-3"
+                        aria-label="Default select example"
+                      >
+                        <option selected>Choose document issued office </option>
+                        <option>Ekantakuna, Lalitpur</option>
+                        <option>Sanobharyang, Kathmandu</option>
+                        <option>Jagati, Bhaktapur</option>
+                      </select>
+                    </div>
+
+                    <div className="input-group flex-nowrap">
+                      <input
+                        type="number"
+                        className="form-control my-3"
+                        placeholder="Enter your document number"
+                        aria-label="Username"
+                        aria-describedby="addon-wrapping"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+};
+
+export default Kycform;
