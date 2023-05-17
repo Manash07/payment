39ed66da1c19e5d3f15758e98e89bbc1cb4e00bb
Manash07 <manashghimire2072@gmail.com>
2023-05-17 14:32:55 +0545

    Unnecessary folders deleted

--- client/src/pages/mockkyc/index.js
+++ client/src/pages/mockkyc/index.js
@@ -1,5 +1,6 @@
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import SecondaryHeader from "@/components/secondarynavbar";
 import { useState } from "react";
 
 const Mockkyc = () => {
@@ -12,74 +13,7 @@ const Mockkyc = () => {
         href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"
       ></link>
       <Header />
-      <section className="header-two">
-        <nav className="navbar navbar-expand-lg htwo">
-          <div className="container">
-            <a class="navbar-brand" href="#">
-              Link 1
-            </a>
-            <a class="navbar-brand" href="#">
-              Link 2
-            </a>
-            <a class="navbar-brand" href="#">
-              Link 3
-            </a>
-            <a class="navbar-brand" href="#">
-              Link 4
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon" />
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link" aria-current="page" href="#">
-                    Option 1
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Option 2
-                  </a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link"
-                    href="#"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Option 3
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link">Option 4</a>
-                </li>
-                <form className="d-flex" role="search">
-                  <input
-                    className="form-control me-2"
-                    type="search"
-                    placeholder="Search"
-                    aria-label="Search"
-                  />
-                </form>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </section>
+      <SecondaryHeader/>
       <section className="kycform my-3">
         <div className="container-fluid row">
           <div className="col-sm-12 col-md-6 col-lg-6 main-card-kyc">
@@ -117,7 +51,7 @@ const Mockkyc = () => {
                 <div className="row">
                   <div className="col-md-6 col-lg-6 col-sm-12">
                     <img
-                      src="/Icon.png"
+                      src="/icon.png"
                       className="img-thumbnail my-3"
                       alt="..."
                       style={{ width: "10rem", height: "10rem" }}
